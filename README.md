91907c00f9c18356f735709e1daeff5d2e0d7ea9
Viacheslav Lucheninov <slava.lucheninov@socialtechnologies.io>
2021-06-21 16:25:45 +0300

    One commit to repo

---
+++ README.md
@@ -0,0 +1,11 @@
+# Check file in aws s3
+
+## language: golang
+
+Library: fmt, log, http, ioutil, yaml and aws
+
+## Steps
+
+- Upload file
+- Check status
+- Delete file
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+const ComfigYaml string = "config.yaml"
+
+type Config struct {
+	Storage struct {
+		S3 struct {
+			BucketName string `yaml:"bucket" envconfig:"STORAGE_S3_BUCKET"`
+			RegionName string `yaml:"region" envconfig:"STORAGE_S3_REGION"`
+			KeyID      string `yaml:"key_id" envconfig:"STORAGE_S3_KEY"`
+			AccessKey  string `yaml:"access_key" envconfig:"STORAGE_S3_SECRET"`
+			ACL        string `yaml:"acl" envconfig:"STORAGE_S3_ACL"`
+		}
+		Files string `yaml: "files" envconfig:"STORAGE_FILES"`
+	}
+}
+
+type Reader interface {
+	Read() (*Config, error)
+}
+
+func main() {
+	var configReader Config
+
+	cfgData, err := ioutil.ReadFile(ComfigYaml)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = yaml.Unmarshal(cfgData, &configReader)
+
+	//fmt.Printf("%s", configReader)
+	fmt.Println("Bucket: ", configReader.Storage.S3.BucketName)
+	// Create a single AWS session
+	sessions, err := session.NewSession(
+		&aws.Config{
+			Region:      aws.String(configReader.Storage.S3.RegionName),
+			Credentials: credentials.NewStaticCredentials(configReader.Storage.S3.KeyID, configReader.Storage.S3.AccessKey, ""),
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Open file
+	testfileupload, err := ioutil.ReadFile(configReader.Storage.Files)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Uploads
+	fmt.Print("Upload file: ")
+	_, err = s3.New(sessions).PutObject(&s3.PutObjectInput{
+		Bucket:               aws.String(configReader.Storage.S3.BucketName),
+		Key:                  aws.String(configReader.Storage.Files),
+		ACL:                  aws.String(configReader.Storage.S3.ACL),
+		Body:                 bytes.NewReader(testfileupload),
+		ContentType:          aws.String(http.DetectContentType(testfileupload)),
+		ContentDisposition:   aws.String("attachment"),
+		ServerSideEncryption: aws.String("AES256"),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("OK")
+	fmt.Print("Check file in bucket: ")
+	checktestfile := &s3.GetObjectInput{
+		Bucket: aws.String(configReader.Storage.S3.BucketName),
+		Key:    aws.String(configReader.Storage.Files),
+	}
+	_, err = s3.New(sessions).GetObject(checktestfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("OK")
+	fmt.Print("Remove file: ")
+	deletetestfile := &s3.DeleteObjectInput{
+		Bucket: aws.String(configReader.Storage.S3.BucketName),
+		Key:    aws.String(configReader.Storage.Files),
+	}
+	_, err = s3.New(sessions).DeleteObject(deletetestfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("OK")
+}
